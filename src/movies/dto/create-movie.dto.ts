bc4d23e87b68c4ff02870230f551e628c4c12f2e
Raphael Gomes <rphdgomes@gmail.com>
2022-07-20 10:30:03 -0300

    feature: create movie dto

--- src/movies/dto/create-movie.dto.ts
+++ src/movies/dto/create-movie.dto.ts
@@ -1,1 +1,30 @@
-export class CreateMovieDto {}
+import {
+  IsDateString,
+  IsNotEmpty,
+  IsNumber,
+  IsString,
+  IsUrl,
+  Max,
+  Min,
+} from 'class-validator';
+
+export class CreateMovieDto {
+  @IsString()
+  @IsNotEmpty()
+  title: string;
+
+  @IsString()
+  @IsNotEmpty()
+  description: string;
+
+  @IsDateString()
+  releaseDate: string;
+
+  @IsNumber({ maxDecimalPlaces: 0 })
+  @Min(0)
+  @Max(5)
+  rating: number;
+
+  @IsUrl()
+  imageUrl: string;
+}
